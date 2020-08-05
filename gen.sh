fd356c8d13960433f2f47dd9a447812683ec2c83
Manfred Touron <94029+moul@users.noreply.github.com>
2020-08-05 10:37:59 +0200

    chore: new website

--- gen.sh
+++ gen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+FORMAT=${1:-mp4}
 mkdir -p gifs
 for website in */; do
     for res in 1000x1000; do
 	(
-	    dest=gifs/$res-$(echo $website | sed 's@/$@@').gif
+	    dest=gifs/$res-$(echo $website | sed 's@/$@@').${FORMAT}
 	    echo $dest
 	    rm -rf $dest
 	    set -xe
