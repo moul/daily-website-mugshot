ef8e205bf59efd94ac91189be6af3f30457268c0
Manfred Touron <94029+moul@users.noreply.github.com>
2020-09-16 18:26:15 +0200

    chore: update script and site list

--- fetch.sh
+++ fetch.sh
@@ -5,12 +5,13 @@ while IFS= read -r url; do
     d=$(date +%s)
     mkdir -p $dir/$d
     (
-	cd $dir/$d
-	set -x
-	pageres "$url" --filename=full
-	pageres "$url" 1920x1080 1366x768 1280x1280 1280x800 1000x1000 800x800 500x500 640x480 480x640 --crop
+	      cd $dir/$d
+        date
+	      set -x
+	      retry -m 1 -t 300 pageres "$url" --filename=full
+        date
+	      retry -m 1 -t 300 pageres "$url" 1920x1080 1366x768 1280x1280 1280x800 1000x1000 800x800 500x500 640x480 480x640 --crop
     )
     echo "DONE $url $(echo $(date +%s)-$d | bc)s"
     sleep .5
 done < sites.txt
-						 
